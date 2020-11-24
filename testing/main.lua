628d93af7f49a2f90ed13127cbe808234d64287c
miksuk28 <74315076+miksuk28@users.noreply.github.com>
2020-11-24 21:30:56 +0100

    (kind of) fixed problem with wide images

--- testing/main.lua
+++ testing/main.lua
@@ -1,26 +1,33 @@
 function love.load()
 	love.window.setFullscreen(true, "desktop")
-
+	
 	w = love.graphics.getWidth()
 	h = love.graphics.getHeight()
+	--image = love.graphics.newImage("6.jpg")
+	dir = "memes"
+	files = love.filesystem.getDirectoryItems(dir)
+	image = love.graphics.newImage("/" .. dir .. "/" .. files[love.math.random(#files)])
 
-	image = love.graphics.newImage("4.png")
+	--time between each image
+	slide_time = 2
 end
 
 function love.update(dt)
+	love.timer.sleep(slide_time)
+	image = love.graphics.newImage("/" .. dir .. "/" .. files[love.math.random(#files)])
+
 	img_w = image:getWidth()
 	img_h = image:getHeight()
 	sx = w / img_w
 	sy = h / img_h
-	ratio_x = sx * img_w
-	ratio_y = sy * img_h
+	ratio = sy * img_h
 end
 
 function love.draw()
-	if img_w > img_h then
+	if img_w <= img_h then
+		love.graphics.draw(image, (w/2) - (ratio / 2), 0, 0, math.min(sx, sy))
+	elseif img_w > img_h then
 		love.graphics.draw(image, 0, 0, 0, math.min(sx, sy))
-	elseif img_w < img_h then
-		love.graphics.draw(image, (w/2) - (ratio_x / 2), 0, 0, math.min(sx, sy))
 	end
 end
 
